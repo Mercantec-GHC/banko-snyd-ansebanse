a46eba0e8ae3911fdb7265c6161e015b1f617043
ansebanse <anni0007@edu.mercantec.dk>
2024-03-01 07:22:45 +0100

    Updated

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿//Det her er de forskellige namespaces fra .NET framework. Det er anvendt til at holde styr på de forskellige klasser og interfacer
+using System;                       //Giver programmet essentialt funktioneliet (Input/Output)
+using System.Collections.Generic;   //Giver programmet lov til at lave collections, altså i dette tilfælde bruger vi det til t lave lister 
+using System.Linq;                  //Sorting...
+using System.Threading;             //Creates and controls a thread, sets its priority, and get its status
 
+//Her så skaber vi klassen som representer bingopladen. Bingopladen har to værdier: "Vores unikke id samt de forskellige rækker.
 class BingoCard
 {
     public string UniqueID { get; }
@@ -15,6 +17,7 @@ class BingoCard
     }
 }
 
+//Her laver jeg det med at få de id til at passe med pladerne, men der er lavet interactivt selvom at jeg har hardkodet de forskellige plader. 
 class Program
 {
     static string GetUserInput(string message)
@@ -29,26 +32,31 @@ class Program
         string plade1UniqueID = GetUserInput("Hvad er dit unikke ID for din første plade: ");
         BingoCard plade1 = new BingoCard(plade1UniqueID, new List<List<string>>
         {
-            new List<string> { "30", "41", "51", "61", "74" },
-            new List<string> { "8", "17", "42", "54", "82" },
-            new List<string> { "9", "18", "28", "68", "78" }
+            new List<string> { " ", "  ", "  ", "30", "41", "51", "61", "74", "  " },
+            new List<string> { "8", "17", "  ", "  ", "42", "54", "  ", "  ", "82" },
+            new List<string> { "9", "18", "28", "  ", "  ", "  ", "68", "78", "  " }
         });
 
         //Lave den anden plader, hvor den først sprøger efter et unikt ID – her skal Jeg selv taste anse.
         string plade2UniqueID = GetUserInput("Hvad er dit unikke ID for din anden plade: ");
         BingoCard plade2 = new BingoCard(plade2UniqueID, new List<List<string>>
         {
-            new List<string> { "2", "10", "31", "41", "51" },
-            new List<string> { "6", "37", "53", "64", "87" },
-            new List<string> { "19", "27", "39", "67", "77" }
+            new List<string> { "2", "10", "  ", "31", "41", "51", "  ", "  ", "  " },
+            new List<string> { "6", "  ", "  ", "37", "  ", "53", "64", "  ", "87" },
+            new List<string> { "2", "19", "27", "39", "  ", "  ", "67", "77", "  " }
         });
 
-        string trukketTal = "0";
-        bool bingo1Achieved = false;
-        bool bingo2Achieved = false;
-        bool fuldPlade = false;
-        int fullLinesCount = 0;
+        //I prinippet kan der blive tilføjet så mange plader som jeg vil, der skal bare hardcodes flere plader som passer til de unikke plader som jeg har på hjemmesiden.
 
+
+        //Her definer jeg en helt masse variabler:
+        string trukketTal = "0";        //Holder på det tal der er blevet trukket
+        bool bingo1Achieved = false;    //Boolean som jeg bruger til at fortælle programmet at der er 1 række
+        bool bingo2Achieved = false;    //Boolean som jeg bruger til at fortælle programmet at der er 2 række
+        bool fuldPlade = false;         //Boolean som jeg bruger til at fortælle programmet at der er fuld plade
+        int fullLinesCount = 0;         //Holder styr på hvornår en række er fuld
+
+        //Kører programmet indtil at vi har fået fuldplade, og lader os indsætte tal indtil da. Hvis tallet dog ikke passer inde paramenterne "isvalidNumber" så giver den besked.
         while (!fuldPlade)
         {
             DisplayPlader(plade1, plade2);
@@ -71,12 +79,14 @@ class Program
         }
     }
 
+    //Sådan vi kun bruger tal mellem 1 og 90
     static bool IsValidNumber(string tal)
     {
         int parsedNumber;
         return int.TryParse(tal, out parsedNumber) && parsedNumber >= 1 && parsedNumber <= 90;
     }
 
+    //Opdater vores plader med kryser 
     static void UpdatePlader(BingoCard plade1, BingoCard plade2, string trukketTal)
     {
         UpdateRække(plade1.Rows);
